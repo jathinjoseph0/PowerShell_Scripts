5877cf0f66732081b3938d9f4523ef5738e13875
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-07 21:54:15 -0700

    Update New_Machine_Setup.ps1
    Set up Git for installation

--- New_Machine_Setup.ps1
+++ New_Machine_Setup.ps1
@@ -5,6 +5,7 @@ winget install rocksdanister.LivelyWallpaper
 winget install lars-berger.GlazeWM
 winget install VSCodium.VSCodium
 winget install Microsoft.PowerToys
+winget install Git.Git
 winget install Python.Python.3.11
 winget install RProject.R
 winget install RProject.Rtools
