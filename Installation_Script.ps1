476d542c92643c3ba94be13a5d6d4eba7dc159b2
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-07 22:06:44 -0700

    Create Installation_Script.ps1
    Split the script into Uninstall and Install files for easier maintenance.

---
+++ Installation_Script.ps1
@@ -0,0 +1,12 @@
+# Installation Phase
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+winget install 'App Installer'
+winget install rocksdanister.LivelyWallpaper
+winget install lars-berger.GlazeWM
+winget install VSCodium.VSCodium
+winget install Microsoft.PowerToys
+winget install Git.Git
+winget install Python.Python.3.11
+winget install RProject.R
+winget install RProject.Rtools
+winget install Posit.RStudio
