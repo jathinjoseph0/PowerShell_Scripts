718aacd45737b14b5f29eeff54fcc4041bbe01c3
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-08 16:05:32 -0400

    Update Installation_Script.ps1
    Corrected issues in GlazeWM install.

--- Installation_Script.ps1
+++ Installation_Script.ps1
@@ -8,14 +8,20 @@ winget upgrade --all
 "Beginning application installs"
 winget install Git.Git
 winget install rocksdanister.LivelyWallpaper
-# GlazeWM Setup Begin
+# Start GlazeWM Setup
+"Begin GlazeWM Install"
+"Create GlazeWM Folder"
 New-Item -ItemType Directory $HOME\GlazeWM\
 Set-Location $HOME\GlazeWM\
+"Downloading GlazeWM portable application"
 curl -o GlazeWM.exe https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe
+"Creating folder for config"
 New-Item -ItemType Directory $HOME\.glaze-wm
 cd $HOME\.glaze-wm
+"Downloading config"
 curl -o config.yaml https://gist.github.com/jathinjoseph0/9849775ee708fbde64c6a97164798f0f/raw/GlazeWM.yaml
 cd $HOME\GlazeWM\
+"Start GlazeWM"
 .\GlazeWM.exe
 # GlazeWM Setup End
 Set-Location $HOME
