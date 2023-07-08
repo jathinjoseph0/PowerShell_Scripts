7b24945540f8627bfa3f848588bc73fa7912479b
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-07 21:35:38 -0700

    Create New_Machine_Setup.ps1

---
+++ New_Machine_Setup.ps1
@@ -0,0 +1,53 @@
+# Installation Phase
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+winget install 'App Installer'
+winget install rocksdanister.LivelyWallpaper
+winget install lars-berger.GlazeWM
+winget install VSCodium.VSCodium
+winget install Microsoft.PowerToys
+winget install Python.Python.3.11
+winget install RProject.R
+winget install RProject.Rtools
+winget install Posit.RStudio
+# Uninstallation Phase
+winget uninstall 'OMEN Audio Control'
+winget uninstall 'OMEN Gaming Hub'
+winget uninstall 'Clipchamp'
+winget uninstall 'Cortana'
+winget uninstall 'Microsoft News'
+winget uninstall 'MSN Weather'
+winget uninstall 'Get Help'
+winget uninstall 'Microsoft Tips'
+winget uninstall 'Office'
+winget uninstall 'Microsoft Solitaire Collection'
+winget uninstall 'Microsoft Sticky Notes'
+winget uninstall 'Paint'
+winget uninstall 'Microsoft People'
+winget uninstall 'Power Automate'
+winget uninstall 'Microsoft To Do'
+winget uninstall 'Microsoft Photos'
+winget uninstall 'Windows Camera'
+winget uninstall 'Windows Clock'
+winget uninstall 'Windows Calculator'
+winget uninstall 'Feedback Hub'
+winget uninstall 'Windows Maps'
+winget uninstall 'Windows Voice Recorder'
+winget uninstall 'Your Phone'
+winget uninstall 'Windows Media Player'
+winget uninstall 'Movies & TV'
+winget uninstall 'Microsoft Family'
+winget uninstall 'Quick Assist'
+winget uninstall 'Microsoft Teams'
+winget uninstall 'Mail and Calendar'
+winget uninstall 'Xbox'
+winget uninstall 'Spotify Music'
+winget uninstall 'Messenger'
+winget uninstall 'Instagram'
+winget uninstall 'Xbox TCUI'
+winget uninstall 'Xbox Game Bar Plugin'
+winget uninstall 'Xbox Identity Provider'
+winget uninstall 'Xbox Game Speech Window'
+winget uninstall 'Snipping Tool'
+winget uninstall 'Netflix'
+winget uninstall 'LinkedIn'
+exit
