6acc48b8e250ba834f25ee5794a4f6fc9a31fc16
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-07 23:26:42 -0700

    Update Installation_Script.ps1
    Added automated setup and configuration for the GlazeWM tiling window manager.

--- Installation_Script.ps1
+++ Installation_Script.ps1
@@ -1,11 +1,20 @@
 # Installation Phase
 Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 winget upgrade --all
+winget install Git.Git
 winget install rocksdanister.LivelyWallpaper
-winget install lars-berger.GlazeWM
+# GlazeWM Setup Begin
+winget install lars-berger.GlazeWM --location $HOME\GlazeWM\
+cd $HOME\GlazeWM\
+Get-ChildItem -Filter "*.exe" | Rename-Item -NewName {"GlazeWM.exe"}
+cd $HOME\.glaze-wm
+curl -o config.yaml https://gist.github.com/jathinjoseph0/9849775ee708fbde64c6a97164798f0f/raw/GlazeWM.yaml
+cd $HOME\GlazeWM\
+.\GlazeWM.exe
+# GlazeWM Setup End
+cd $HOME
 winget install VSCodium.VSCodium
 winget install Microsoft.PowerToys
-winget install Git.Git
 winget install Python.Python.3.11
 winget install RProject.R
 winget install RProject.Rtools
