ed7a4bbf2058fa1f677801d502cccbed05853fd8
jathinjoseph0 <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-08 21:47:11 -0400

    Create Rollback_Installation.ps1

---
+++ Rollback_Installation.ps1
@@ -0,0 +1,12 @@
+# Rollback Begin
+winget uninstall Git.Git
+winget uninstall rocksdanister.LivelyWallpaper
+# Begin GlazeWM Uninstall
+Remove-Item $HOME\GlazeWM\ -Recurse
+Remove-Item $HOME\.glaze-wm\ -Recurse
+# Finish GlazeWM Uninstall
+winget uninstall VSCodium.VSCodium
+winget uninstall Microsoft.PowerToys
+
+stop-process -name powershell
+stop-process -name WindowsTerminal
