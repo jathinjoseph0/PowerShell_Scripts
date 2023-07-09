93283fd939c3dfd6d2359b5961ca5a8de3c88cd8
Jathin Joseph <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-09 15:42:49 -0400

    Update Installation_Script.ps1
    Starting generation of a modular version of the script.

--- Installation Scripts/Installation_Script.ps1
+++ Installation Scripts/Installation_Script.ps1
@@ -7,25 +7,12 @@ winget update
 winget upgrade --all
 "Beginning application installs"
 winget install Git.Git
-winget install rocksdanister.LivelyWallpaper
-# Start GlazeWM Setup
-"Begin GlazeWM Install"
-"Create GlazeWM Folder"
-New-Item -ItemType Directory $HOME\GlazeWM\
-Set-Location $HOME\GlazeWM\
-"Downloading GlazeWM portable application"
-curl -o GlazeWM.exe https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe
-"Creating folder for config"
-New-Item -ItemType Directory $HOME\.glaze-wm
-cd $HOME\.glaze-wm
-"Downloading config"
-curl -o config.yaml https://gist.github.com/jathinjoseph0/9849775ee708fbde64c6a97164798f0f/raw/GlazeWM.yaml
-cd $HOME\GlazeWM\
-"Start GlazeWM"
-.\GlazeWM.exe
-# GlazeWM Setup End
-Set-Location $HOME
 winget install Microsoft.PowerToys
+winget install rocksdanister.LivelyWallpaper
+curl -o GlazeWM.ps1 https://gist.github.com/jathinjoseph0/3c7ac3d2cccdf781508ea8f849a36866/raw/GlazeWM_Installer.ps1
+.\GlazeWM.ps1
+Set-Location $HOME
+
 
 stop-process -name powershell
 stop-process -name WindowsTerminal
