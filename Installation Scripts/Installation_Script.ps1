536850f80a30e3f9bc6c339352d237004ba5f269
Jathin Joseph <122744074+jathinjoseph0@users.noreply.github.com>
2023-07-09 18:25:08 -0700

    Update Bloat remover Script

--- Installation Scripts/Installation_Script.ps1
+++ Installation Scripts/Installation_Script.ps1
@@ -9,6 +9,7 @@ winget upgrade --all
 winget install Git.Git
 winget install Microsoft.PowerToys
 winget install rocksdanister.LivelyWallpaper
+winget install Microsoft.PowerShell
 curl -o GlazeWM.ps1 https://gist.github.com/jathinjoseph0/3c7ac3d2cccdf781508ea8f849a36866/raw/GlazeWM_Installer.ps1
 .\GlazeWM.ps1
 Set-Location $HOME
